97c6d9e184de6ee313e4d1fe717398c02f237fab
nate-d-olson <nolson@nist.gov>
2015-08-12 22:54:34 -0400

    onLoad assumes datadata in data dir

--- R/zzz.R
+++ R/zzz.R
@@ -2,22 +2,20 @@
 ### Load greenGenesDB into namespace
 ###
 
-.onAttach <- function(libname, pkgname)
+.onLoad <- function(libname, pkgname)
 {
     ns <- asNamespace(pkgname)
     path <- system.file("extdata", package=pkgname, lib.loc=libname)
-    seq_file <- system.file("extdata", "gg_13_5.fasta.gz",
-                          package=pkgname, lib.loc=libname)
-    if(!file.exists(seq_file)){
-        print("Green genes 13.5 database sequence data not present, use `getGreenGenes13.5Db()` to dowload database.")
-        return()
-    }
+    seq_file <- system.file("data", "gg_13_5.fasta.gz",
+                            package=pkgname, lib.loc=libname)
 
-    db_taxa_file <- system.file("extdata", "gg_13_5.sqlite3",
+    db_taxa_file <- system.file("data", "gg_13_5.sqlite3",
                                 package=pkgname, lib.loc=libname)
-    if(!file.exists(db_taxa_file)){
-        print("Green genes 13.5 taxonomy database not present, use `getGreenGenes13.5Db()` to dowload database")
-        return()
+
+
+    if(!file.exists(seq_file) || !file.exists(db_taxa_file)){
+        print("Green genes 13.5 database data not present, use `getGreenGenes13.5Db.R` In the package inst/scripts directory to downlod the database into the package data/ directory and reinstall the package")
+        ## not sure this is the best error message
     }
 
     metadata = list(URL = "https://greengenes.microbio.me",
@@ -29,13 +27,13 @@
     db_seq <- Biostrings::readDNAStringSet(seq_file)
 
     ## load taxa sqlite database
-    db_taxa_file <- system.file("extdata", "gg_13_5.sqlite3",
+    db_taxa_file <- system.file("data", "gg_13_5.sqlite3",
                                 package=pkgname, lib.loc=libname)
 
     ## initiate new MgDB object
     ggMgDb <- new("MgDb",seq = db_seq,
-        taxa = db_taxa_file,
-        metadata = metadata)
+                  taxa = db_taxa_file,
+                  metadata = metadata)
 
     assign("gg13.5MgDb", ggMgDb, envir=ns)
     namespaceExport(ns, "gg13.5MgDb")
