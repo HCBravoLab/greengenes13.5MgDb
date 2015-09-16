7d68988d994292e843daee5466351b1734570c6a
nate-d-olson <nolson@nist.gov>
2015-09-16 13:57:13 -0400

    added documentation

---
+++ R/gg13.5MgDb.R
@@ -0,0 +1,13 @@
+###############################################
+#'  MgDb-class object for greengenes version 13.5
+#'
+#' gg13.5MgDb is a MgDb-class object with
+#' sequence and taxonomic data for the greengenes
+#' database version 13.5, \url{http://greengenes.secondgenome.com}
+#'
+#' @name gg13.5MgDb
+#' @seealso \link[metagenomeFeatures]{MgDb-class}
+#' @examples
+#' gg13.5MgDb
+NULL
+###############################################
---
+++ R/greengenes13.5MgDb.R
@@ -0,0 +1,24 @@
+###############################################
+#'  Greengenes 16S rRNA database MgDb Package.
+#'
+#' This package loads a MgDb class object with
+#' sequence and taxonomic data for the greengenes
+#' database version 13.5, \url{http://greengenes.secondgenome.com}
+#'
+#' @name greengenes13.5MgDb-package
+#' @author Nathan D. Olson \email{nolson@@umiacs.umd.edu}
+#' @docType package
+#' @keywords package
+#' @seealso \link[metagenomeFeatures]{MgDb-class}
+#' @importClassesFrom metagenomeFeatures MgDb
+#' @importClassesFrom Biostrings DNAStringSet
+#' @importFrom Biostrings readDNAStringSet
+#' @import dplyr
+#' @import RSQLite
+#' @import metagenomeFeatures
+#' @examples
+#' library(greengenes13.5MgDb)
+#' ls('package:greengenes13.5MgDb')
+#' gg13.5MgDb
+NA
+###############################################
