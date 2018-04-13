0cd163376a8e69d40cf91cdbc1b9e619669fe6b2
Nate Olson <nolson@nist.gov>
2018-04-13 08:08:15 -0400

    edits for BiocCheck

--- R/zzz.R
+++ R/zzz.R
@@ -13,7 +13,11 @@
     ## Note no tree for gg13.5
 
     if (!file.exists(db_file) | !file.exists(metadata_file)) {
-        packageStartupMessage("Greengenes 13.5 database data not present, use `get_greengenesDb.R` In the package inst/scripts directory to download the database into the package inst/extdata/ directory and reinstall the package")
+        packageStartupMessage("Greengenes 13.5 database data not present, ",
+                              "use `get_greengenesDb.R` In the package ",
+                              "inst/scripts directory to download the ",
+                              "database into the package inst/extdata/ ",
+                              "directory and reinstall the package")
     }
 }
 
@@ -32,8 +36,8 @@
 
     ## initiate new MgDB object
     ggMgDb <- metagenomeFeatures::newMgDb(db_file = db_file,
-                      tree = NULL,
-                      metadata = metadata)
+                                          tree = NULL,
+                                          metadata = metadata)
 
     assign("gg13.5MgDb", ggMgDb, envir = ns)
     namespaceExport(ns, "gg13.5MgDb")
